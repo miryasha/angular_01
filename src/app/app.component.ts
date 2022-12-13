baa530aca890d0ea483cacfb5db8e10d26e56139
asgharm <asgharm@enterprise.trin.net>
2022-12-13 08:42:59 -0600

    adding some new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [` h3 {
+  //   color: red;
+  // }`]
 })
 export class AppComponent {
   title = 'basic_project_bootstarp';
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,20 @@
 import { Component } from "@angular/core";
 
+
 @Component( {
     selector: 'app-server',
     templateUrl: './server.component.html',
     styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    serverID: number = 10;
+    serverStatus: string = 'offline';
+
+
+    getServerStatus(): string {
+        return this.serverStatus
+    }
+
+
 
 }
